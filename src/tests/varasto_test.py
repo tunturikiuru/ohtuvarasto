a0f554fa1b2cd9b00ea9b0f1cfbae091934f857a
Anna Salminiitty <koivu05@gmail.com>
2024-10-24 18:25:59 +0300

    tehtävä 8

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -2,6 +2,8 @@ import unittest
 from varasto import Varasto
 
 
+
+
 class TestVarasto(unittest.TestCase):
     def setUp(self):
         self.varasto = Varasto(10)
@@ -38,3 +40,42 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+    #uudet testit
+    #7, 11, 17, 25, 29, 33, 35-38, 45
+
+    def test_negatiivinen_tilavuus(self):
+        varasto = Varasto(-2)
+        self.assertAlmostEqual(varasto.tilavuus, 0)
+
+    def test_negatiivinen_alkusaldo(self):
+        varasto = Varasto(10, -1)
+        self.assertAlmostEqual(varasto.saldo, 0)
+
+    def test_alkusaldo_ylittaa_tilavuuden(self):
+        varasto = Varasto(10, 12)
+        self.assertAlmostEqual(varasto.saldo, 10)
+
+    def test_lisataan_negatiivinen_maara(self):
+        varasto = Varasto(10, 8)
+        varasto.lisaa_varastoon(-2)
+        self.assertAlmostEqual(varasto.saldo, 8)
+
+    def test_lisataan_ylimaara(self):
+        self.varasto.lisaa_varastoon(12)
+        self.assertAlmostEqual(self.varasto.saldo, 10)
+
+    def test_otetaan_negatiivinen_maara(self):
+        self.varasto.lisaa_varastoon(8)
+        saatu_maara = self.varasto.ota_varastosta(-2)
+        self.assertAlmostEqual(saatu_maara, 0)
+
+    def test_otetaan_enemman_kuin_saldo(self):
+        self.varasto.lisaa_varastoon(8)
+        saatu_maara = self.varasto.ota_varastosta(10)
+        self.assertAlmostEqual(saatu_maara, 8)
+
+    def test_tulostus_oikein(self):
+        self.varasto.lisaa_varastoon(2)
+        vastaus = str(self.varasto)
+        self.assertAlmostEqual(vastaus, "saldo = 2, vielä tilaa 8")
