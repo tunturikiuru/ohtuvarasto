cd4af3d429a362f2e49c35a4ad8a53c89b226864
Anna Salminiitty <koivu05@gmail.com>
2024-10-26 20:48:02 +0300

    viikko2 teht3

--- src/index.py
+++ src/index.py
@@ -5,6 +5,16 @@ def main():
     mehua = Varasto(100.0)
     olutta = Varasto(100.0, 20.2)
 
+    # pylintin takia
+    # tehtävänannossa ei sanottu, että tämän voi jättää huomiotta
+
+    part1(olutta, mehua)
+    part2(mehua)
+    part3()
+    part4(olutta, mehua)
+    part5(olutta, mehua)
+
+def part1(olutta, mehua):
     print("Luonnin jälkeen:")
     print(f"Mehuvarasto: {mehua}")
     print(f"Olutvarasto: {olutta}")
@@ -14,6 +24,7 @@ def main():
     print(f"tilavuus = {olutta.tilavuus}")
     print(f"paljonko_mahtuu = {olutta.paljonko_mahtuu()}")
 
+def part2(mehua):
     print("Mehu setterit:")
     print("Lisätään 50.7")
     mehua.lisaa_varastoon(50.7)
@@ -22,6 +33,7 @@ def main():
     mehua.ota_varastosta(3.14)
     print(f"Mehuvarasto: {mehua}")
 
+def part3():
     print("Virhetilanteita:")
     print("Varasto(-100.0);")
     huono = Varasto(-100.0)
@@ -31,6 +43,7 @@ def main():
     huono = Varasto(100.0, -50.7)
     print(huono)
 
+def part4(olutta, mehua):
     print(f"Olutvarasto: {olutta}")
     print("olutta.lisaa_varastoon(1000.0)")
     olutta.lisaa_varastoon(1000.0)
@@ -41,6 +54,7 @@ def main():
     mehua.lisaa_varastoon(-666.0)
     print(f"Mehuvarasto: {mehua}")
 
+def part5(olutta, mehua):
     print(f"Olutvarasto: {olutta}")
     print("olutta.ota_varastosta(1000.0)")
     saatiin = olutta.ota_varastosta(1000.0)
--- src/varasto.py
+++ src/varasto.py
@@ -16,7 +16,8 @@ class Varasto:
             # täyteen ja ylimäärä hukkaan!
             self.saldo = tilavuus
 
-    # huom: ominaisuus voidaan myös laskea. Ei tarvita erillistä kenttää viela_tilaa tms.
+
+    # huom: ominaisuus voidaan myös laskea. Ei tarvita erillistä kenttää
     def paljonko_mahtuu(self):
         return self.tilavuus - self.saldo
 
@@ -43,5 +44,3 @@ class Varasto:
 
     def __str__(self):
         return f"saldo = {self.saldo}, vielä tilaa {self.paljonko_mahtuu()}"
-    
-
