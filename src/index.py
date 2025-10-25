0bf6258b9a332425a5677975190cba68dbaa2338
Anna Salminiitty <koivu05@gmail.com>
2025-10-25 20:38:10 +0300

    1.11

--- src/index.py
+++ src/index.py
@@ -5,6 +5,7 @@ def main():
     mehua = Varasto(100.0)
     olutta = Varasto(100.0, 20.2)
 
+    print('uusi rivi')
     print("Luonnin jälkeen:")
     print(f"Mehuvarasto: {mehua}")
     print(f"Olutvarasto: {olutta}")
