c2e9513c71a6289a4317fe1c419d0a4e17f77121
Anna Salminiitty <koivu05@gmail.com>
2025-10-29 22:00:10 +0200

    2.7 korjaus

--- src/index.py
+++ src/index.py
@@ -5,7 +5,7 @@ def main():
     mehua = Varasto(100.0)
     olutta = Varasto(100.0, 20.2)
 
-    print('Olut getterit:')
+    print("Olut getterit:")
     print(f"saldo = {olutta.saldo}")
     print(f"tilavuus = {olutta.tilavuus}")
     print(f"paljonko_mahtuu = {olutta.paljonko_mahtuu()}")
