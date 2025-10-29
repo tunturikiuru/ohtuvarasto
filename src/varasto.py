0e245a47ae9d00ff9e3fb96626bc26f380720e8d
Anna Salminiitty <koivu05@gmail.com>
2025-10-29 21:49:13 +0200

    2.6

--- src/varasto.py
+++ src/varasto.py
@@ -1,22 +1,14 @@
 class Varasto:
     def __init__(self, tilavuus, alku_saldo = 0):
-        if tilavuus > 0.0:
-            self.tilavuus = tilavuus
-        else:
-            # virheellinen, nollataan
-            self.tilavuus = 0.0
+        self.tilavuus = tilavuus if tilavuus > 0.0 else 0.0
 
         if alku_saldo < 0.0:
-            # virheellinen, nollataan
             self.saldo = 0.0
         elif alku_saldo <= tilavuus:
-            # mahtuu
             self.saldo = alku_saldo
         else:
-            # täyteen ja ylimäärä hukkaan!
             self.saldo = tilavuus
 
-    # huom: ominaisuus voidaan myös laskea. Ei tarvita erillistä kenttää viela_tilaa tms.
     def paljonko_mahtuu(self):
         return self.tilavuus - self.saldo
 
